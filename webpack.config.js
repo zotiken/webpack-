71aa834b3445d9ab719bb0a1324154a471364991
zotiken <neksklad@mail.ru>
2020-06-25 12:09:47 +0300

    add plugin webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,25 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+const MODE = process.env.MODE || "development";
+
 module.exports = {
-  entry:{
+  mode: MODE,
+  entry: {
     main: "./src/index.js",
-    analytics: "./src/analytics.js"
-},
+    analytics: "./src/analytics.js",
+  },
   output: {
     filename: "[name].[hash].js",
     path: path.resolve(__dirname, "dist"),
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/index.html"),
+    }),
+    new FaviconsWebpackPlugin('./src/favicon.ico'),
+    new CleanWebpackPlugin()
+  ],
 };
