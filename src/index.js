b0b101ffbc15450415fc9dce0d34d1c62e060bc1
zotiken <neksklad@mail.ru>
2020-06-25 13:24:26 +0300

    add loader style

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import Post from "./Post"
+import img from './assets/webpack-logo.png';
+import './styles/style.css';
+import './styles/style.scss';
 
 const post = new Post ("Webpack new post");
 
-console.log(post.toString());
+console.log(post.toString(),img);
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MODE = process.env.MODE || "development";
 
 module.exports = {
-  mode: MODE,
+  mode: "development",
   entry: {
     main: "./src/index.js",
     analytics: "./src/analytics.js",
@@ -22,4 +22,32 @@ module.exports = {
     new FaviconsWebpackPlugin('./src/favicon.ico'),
     new CleanWebpackPlugin()
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+
+    ],
+  },
 };
